20e50876f6bc81ff4f0606386d87b61c57162497
jazzc001 <jazzcheung212@gmail.com>
2022-05-23 00:19:58 +0100

    seperate pagination file to query.js

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -112,11 +112,11 @@ async function getLatestFlightNumber() {
     return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
+async function getAllLaunches(skip, limit) {
     return await launchesDatabase
         .find({}, {'__id': 0, '__v': 0})
-        .skip(20)
-        .limit(50);
+        .skip(skip)
+        .limit(limit);
 }
 
 async function saveLaunch(launch) {
